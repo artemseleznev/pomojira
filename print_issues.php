96a6d65f26aa9ded27fab8396949fbd2d97edea8
seleznev <seleznev@tutu.ru>
2017-11-24 14:28:48 +0300

    styles fix after jira update

--- print_issues.php
+++ print_issues.php
@@ -28,10 +28,10 @@ foreach ($issueList as $issueKey)
 <head>
 	<title>Print jira issues</title>
 </head>
-<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/f86ae594ebbf179ede10d2eee7b3bd78-CDN/ljarqa/72005/e2b820449dba7db3e64153a92911b4be/28f4022e59967068c4f033a45fdbe612/_/download/contextbatch/css/_super/batch.css?atlassian.aui.raphael.disabled=true" data-wrm-key="_super" data-wrm-batch-type="context" media="all">
-<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/12481e7c867955039984da8a455cfb20-CDN/ljarqa/72005/e2b820449dba7db3e64153a92911b4be/785da28a61efbef21cad6163b341edd3/_/download/contextbatch/css/greenhopper-rapid-non-gadget,atl.general,gh-rapid,jira.project.sidebar,com.atlassian.jira.projects.sidebar.init,jira.global,jira.general,-_super/batch.css?agile_global_admin_condition=true&amp;atlassian.aui.raphael.disabled=true&amp;hc-enabled=true&amp;is-server-instance=true&amp;jag=true&amp;jaguser=true&amp;nps-not-opted-out=true&amp;sd_operational=true" data-wrm-key="greenhopper-rapid-non-gadget,atl.general,gh-rapid,jira.project.sidebar,com.atlassian.jira.projects.sidebar.init,jira.global,jira.general,-_super" data-wrm-batch-type="context" media="all">
-<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/36f98085b58aa80d794a3b54018eda21-CDN/ljarqa/72005/e2b820449dba7db3e64153a92911b4be/db2d903ac476c291ba8e898fcd04e603/_/download/contextbatch/css/gh-rapid-charts,-_super/batch.css?atlassian.aui.raphael.disabled=true" data-wrm-key="gh-rapid-charts,-_super" data-wrm-batch-type="context" media="all">
-<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/2a21883ce1363b7b74a655e959c64c5c-T/ljarqa/72005/e2b820449dba7db3e64153a92911b4be/7.2.3/_/download/batch/com.atlassian.feedback.jira-feedback-plugin:button-resources-init/com.atlassian.feedback.jira-feedback-plugin:button-resources-init.css" data-wrm-key="com.atlassian.feedback.jira-feedback-plugin:button-resources-init" data-wrm-batch-type="resource" media="all">
+<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/a3fc6c7eda193d15bf35cbfeecbdd86f-CDN/-1eamx9/75007/e2b820449dba7db3e64153a92911b4be/aa6cf545ff213915ecbf1d6cdcbaa2cc/_/download/contextbatch/css/_super/batch.css" data-wrm-key="_super" data-wrm-batch-type="context" media="all">
+<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/f6e9358fbf5ece8e8fb6a17329d714f3-CDN/-1eamx9/75007/e2b820449dba7db3e64153a92911b4be/4ab0e2fb52a2d56d6012a4925948cff4/_/download/contextbatch/css/greenhopper-rapid-non-gadget,atl.general,gh-rapid,jira.project.sidebar,com.atlassian.jira.projects.sidebar.init,jira.global,jira.general,-_super/batch.css?agile_global_admin_condition=true&amp;hc-enabled=true&amp;is-server-instance=true&amp;jag=true&amp;jaguser=true&amp;nps-not-opted-out=true&amp;richediton=true&amp;sd_operational=true" data-wrm-key="greenhopper-rapid-non-gadget,atl.general,gh-rapid,jira.project.sidebar,com.atlassian.jira.projects.sidebar.init,jira.global,jira.general,-_super" data-wrm-batch-type="context" media="all">
+<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/32d154aa5ed37a219ff9cd15b0a8a7d1-CDN/-1eamx9/75007/e2b820449dba7db3e64153a92911b4be/95500190d36d26962b40eba9cc2f98e5/_/download/contextbatch/css/gh-rapid-charts,-_super/batch.css" data-wrm-key="gh-rapid-charts,-_super" data-wrm-batch-type="context" media="all">
+<link type="text/css" rel="stylesheet" href="https://hq.tutu.ru/s/b0798f450f70bcb060fee4ed5fc98bf0-T/-1eamx9/75007/e2b820449dba7db3e64153a92911b4be/7.5.2/_/download/batch/com.atlassian.feedback.jira-feedback-plugin:button-resources-init/com.atlassian.feedback.jira-feedback-plugin:button-resources-init.css" data-wrm-key="com.atlassian.feedback.jira-feedback-plugin:button-resources-init" data-wrm-batch-type="resource" media="all">
 <style type="text/css">
 	@media print { body { -webkit-print-color-adjust: exact; } }
 </style>
@@ -47,19 +47,19 @@ foreach ($issueList as $issueKey)
 
 	.ghx-print-card-body .ghx-print-large .ghx-card {
 		height: 100%;
-		padding: 20px 10px 35px 35px;
-		width: 620px;
+		padding: 20px 10px 20px 35px;
+		width: 480px;
 	}
 
 	.ghx-card-summary {
 		margin-top: 30px !important;
-		font-size: 45px !important;
+		/*font-size: 35px !important;*/
 		line-height: 1em !important;
 	}
 
 	img.ghx-avatar-img {
-		width: 80px !important;
-		height: 80px !important;
+		width: 60px !important;
+		height: 60px !important;
 	}
 
 	.tester-wrapper {
@@ -75,10 +75,10 @@ foreach ($issueList as $issueKey)
 	.tester {
 		display: inline-block;
 		width: 170px;
-		margin-left: 90px;
+		margin-left: 18px;
 		height: 100%;
 		vertical-align: middle;
-		margin-top: -15px;
+		margin-top: -5px;
 	}
 
 	.tester table {
@@ -92,8 +92,8 @@ foreach ($issueList as $issueKey)
 	}
 
 	.tester-text {
-		width: 40px;
-		height: 35px;
+		width: 25px;
+		height: 33px;
 		margin: 10px;
 		max-width: 40px;
 		overflow: hidden;
@@ -132,7 +132,7 @@ foreach ($issueList as $issueKey)
 		<div class="ghx-card ">
 			<div class="ghx-card-content">
 				<div class="ghx-card-header">
-					<div class="ghx-card-key" style="font-size: 58px; font-weight: bold;">
+					<div class="ghx-card-key" style="font-size: 48px; font-weight: bold;">
 						<?= $item['issueKey']; ?>
 					</div>
 					<div class="tester">
@@ -145,7 +145,7 @@ foreach ($issueList as $issueKey)
 						<img src="<?=$item['assigneeAvatarLink'];?>" class="ghx-avatar-img" style="border-radius: 25px; height: 100px; line-height: 50px; width: 100px;">
 					</div>
 				</div>
-				<div class="ghx-card-summary" style="font-size: 53px; line-height: 1.3em; max-height: none; margin-top: 22px; font-weight: 100;">
+				<div class="ghx-card-summary" style="font-size: 32px; line-height: 1.3em; max-height: none; margin-top: 20px !important; font-weight: 100;">
 					<?= $item['summary']; ?>
 					<span class="ellipsis">…</span>
 					<span class="obscurer"> </span>
